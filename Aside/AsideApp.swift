73d0e7e3cbd093c1688649fd3800c5e1d89499d4
Jack Devey <jack@jw3.uk>
2022-08-01 22:10:02 +0100

    Initial Commit

---
+++ Aside/AsideApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsideApp.swift
+//  Aside
+//
+//  Created by Jack Devey on 01/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct AsideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aside/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Aside
+//
+//  Created by Jack Devey on 01/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
